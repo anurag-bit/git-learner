72058a9447c0b95c01e16e84971170c13d407c9e
Anurag Kumar Singh <singh.ask@outlook.com>
2024-09-26 16:01:50 +0530

    added: blob ingestion and reading!

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 // Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Interactive Git Learning Environment By Anurag")
 
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
@@ -31,6 +34,19 @@ func main() {
 
 		fmt.Println("Initialized git directory")
 
+	case "cat-file":
+		sha := os.Args[3]
+		path := fmt.Sprintf(".git/objects/%v/%v", sha[0:2], sha[2:])
+		file, _ := os.Open(path)
+		r, _ := zlib.NewReader(io.Reader(file))
+		s, _ := io.ReadAll(r)
+		parts := strings.Split(string(s), "\x00")
+		fmt.Print(parts[1])
+		err := r.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error closing file: %s\n", err)
+			return
+		}
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
